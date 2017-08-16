9e0283261608339cae31afedd75d15eb24f6f7d4
Pushpraj Ruhal <pur@sitecore.net>
2017-08-16 09:39:04 +0800

    updated SqlSessionStateProvider code for 8.2.4

--- src/Sitecore.Support.96296/SqlSessionStateProvider.cs
+++ src/Sitecore.Support.96296/SqlSessionStateProvider.cs
@@ -1,163 +1,274 @@
 ﻿namespace Sitecore.Support.SessionProvider.Sql
 {
-    using Sitecore.Diagnostics;
-    using Sitecore.SessionProvider;
-    using Sitecore.SessionProvider.Helpers;
-    using System;
-    using System.Collections.Specialized;
-    using System.Configuration;
-    using System.Diagnostics;
-    using System.Runtime.InteropServices;
-    using System.Web;
-    using System.Web.SessionState;
+  using System;
+  using System.Collections.Generic;
+  using System.Collections.Specialized;
+  using System.Configuration;
+  using System.Diagnostics;
+  using System.Linq;
+  using System.Web;
+  using System.Web.Configuration;
+  using System.Web.SessionState;
+  using Sitecore.Diagnostics;
+  using Sitecore.SessionProvider.Helpers;
+  using Sitecore.SessionProvider;
 
-    public class SqlSessionStateProvider : SitecoreSessionStateStoreProvider
+  public class SqlSessionStateProvider : SitecoreSessionStateStoreProvider
+  {
+    #region Fields
+
+    private Guid m_ApplicationId;
+
+    private Sitecore.Support.SessionProvider.Sql.SqlSessionStateStore m_Store;
+
+    private static readonly List<SqlSessionStateProvider> SqlSessionStateProvidersList =
+      new List<SqlSessionStateProvider>();
+
+    private static readonly object ListSyncRoot = new object();
+
+    #endregion
+
+    #region Constructors
+
+    static SqlSessionStateProvider()
     {
-        private Guid m_ApplicationId;
-        private Sitecore.Support.SessionProvider.Sql.SqlSessionStateStore m_Store;
-
-        static SqlSessionStateProvider()
-        {
-            Trace.WriteLine("SQL Session State Provider is initializing.", "SqlSessionStateProvider");
-        }
-
-        public override void CreateUninitializedItem(HttpContext context, string id, int timeout)
-        {
-            Sitecore.Diagnostics.Assert.ArgumentNotNull(context, "context");
-            Sitecore.Diagnostics.Assert.ArgumentNotNull(id, "id");
-            HttpStaticObjectsCollection staticObjects = new HttpStaticObjectsCollection();
-            SessionStateStoreData sessionState = new SessionStateStoreData(new SessionStateItemCollection(), staticObjects, timeout);
-            this.m_Store.InsertItem(this.ApplicationId, id, 1, sessionState);
-        }
-
-        protected override SessionStateStoreData GetExpiredItemExclusive(DateTime signalTime, SessionStateLockCookie lockCookie, out string id)
-        {
-            return this.Store.GetExpiredItemExclusive(this.ApplicationId, lockCookie, out id);
-        }
-
-        public override SessionStateStoreData GetItem(HttpContext context, string id, out bool locked, out TimeSpan lockAge, out object lockId, out SessionStateActions actions)
-        {
-            Sitecore.Diagnostics.Assert.ArgumentNotNull(context, "context");
-            Sitecore.Diagnostics.Assert.ArgumentNotNull(id, "id");
-            locked = false;
-            lockAge = TimeSpan.Zero;
-            lockId = null;
-            actions = SessionStateActions.None;
-            int flags = 0;
-            SessionStateLockCookie lockCookie = null;
-            actions = (SessionStateActions)flags;
-            if (lockCookie != null)
-            {
-                locked = true;
-                lockId = lockCookie.Id;
-                lockAge = (TimeSpan)(DateTime.UtcNow - lockCookie.Timestamp);
-            }
-            return this.Store.GetItem(this.ApplicationId, id, out lockCookie, out flags);
-        }
-
-        public override SessionStateStoreData GetItemExclusive(HttpContext context, string id, out bool locked, out TimeSpan lockAge, out object lockId, out SessionStateActions actions)
-        {
-            Sitecore.Diagnostics.Assert.ArgumentNotNull(context, "context");
-            Sitecore.Diagnostics.Assert.ArgumentNotNull(id, "id");
-            lockAge = TimeSpan.Zero;
-            actions = SessionStateActions.None;
-            int flags = 0;
-            SessionStateLockCookie existingLockCookie = null;
-            SessionStateLockCookie acquiredLockCookie = SessionStateLockCookie.Generate(DateTime.UtcNow);
-            SessionStateStoreData data = this.Store.GetItemExclusive(this.ApplicationId, id, acquiredLockCookie, out existingLockCookie, out flags);
-            if (existingLockCookie != null)
-            {
-                locked = true;
-                lockAge = (TimeSpan)(DateTime.UtcNow - existingLockCookie.Timestamp);
-                lockId = existingLockCookie.Id;
-                return data;
-            }
-            locked = false;
-            lockId = acquiredLockCookie.Id;
-            actions = (SessionStateActions)flags;
-            return data;
-        }
-
-        public override void Initialize(string name, NameValueCollection config)
-        {
-            Sitecore.Diagnostics.Assert.ArgumentNotNull(name, "name");
-            Sitecore.Diagnostics.Assert.ArgumentNotNull(config, "config");
-            base.Initialize(name, config);
-            ConfigReader reader1 = new ConfigReader(config, name);
-            string str = reader1.GetString("sessionType", true);
-            string str2 = reader1.GetString("connectionStringName", false);
-            string connectionString = ConfigurationManager.ConnectionStrings[str2].ConnectionString;
-            bool @bool = reader1.GetBool("compression", false);
-            this.m_Store = new Sitecore.Support.SessionProvider.Sql.SqlSessionStateStore(connectionString, @bool);
-            this.m_ApplicationId = this.m_Store.GetApplicationIdentifier(str);
-        }
-
-        public override void ReleaseItemExclusive(HttpContext context, string id, object lockId)
-        {
-            Sitecore.Diagnostics.Assert.ArgumentNotNull(context, "context");
-            Sitecore.Diagnostics.Assert.ArgumentNotNull(id, "id");
-            Sitecore.Diagnostics.Assert.ArgumentNotNull(lockId, "lockId");
-            string lockCookie = Convert.ToString(lockId);
-            this.Store.ReleaseItem(this.ApplicationId, id, lockCookie);
-        }
-
-        protected override void RemoveItem(string id, string lockCookie)
-        {
-            this.Store.RemoveItem(this.ApplicationId, id, lockCookie);
-        }
-
-        public override void RemoveItem(HttpContext context, string id, object lockId, SessionStateStoreData item)
-        {
-            Sitecore.Diagnostics.Assert.ArgumentNotNull(context, "context");
-            Sitecore.Diagnostics.Assert.ArgumentNotNull(id, "id");
-            Sitecore.Diagnostics.Assert.ArgumentNotNull(lockId, "lockId");
-            string lockCookie = Convert.ToString(lockId);
-            try
-            {
-                base.ExecuteSessionEnd(id, item);
-            }
-            finally
-            {
-                this.Store.RemoveItem(this.ApplicationId, id, lockCookie);
-            }
-        }
-
-        public override void ResetItemTimeout(HttpContext context, string id)
-        {
-            Sitecore.Diagnostics.Assert.ArgumentNotNull(context, "context");
-            Sitecore.Diagnostics.Assert.ArgumentNotNull(id, "id");
-            this.Store.UpdateItemExpiration(this.ApplicationId, id);
-        }
-
-        public override void SetAndReleaseItemExclusive(HttpContext context, string id, SessionStateStoreData sessionState, object lockId, bool newItem)
-        {
-            Sitecore.Diagnostics.Assert.ArgumentNotNull(context, "context");
-            Sitecore.Diagnostics.Assert.ArgumentNotNull(id, "id");
-            if (newItem)
-            {
-                this.Store.InsertItem(this.ApplicationId, id, 0, sessionState);
-            }
-            else
-            {
-                string lockCookie = Convert.ToString(lockId);
-                this.Store.UpdateAndReleaseItem(this.ApplicationId, id, lockCookie, SessionStateActions.None, sessionState);
-            }
-        }
-
-        private Guid ApplicationId
-        {
-            get
-            {
-                return this.m_ApplicationId;
-            }
-        }
-
-        private Sitecore.Support.SessionProvider.Sql.SqlSessionStateStore Store
-        {
-            get
-            {
-                return this.m_Store;
-            }
-        }
+      Trace.WriteLine("SQL Session State Provider is initializing.", "SqlSessionStateProvider");
     }
+
+    #endregion
+
+    #region Private properties
+
+    private Guid ApplicationId
+    {
+      get { return this.m_ApplicationId; }
+    }
+
+    [NotNull]
+    private Sitecore.Support.SessionProvider.Sql.SqlSessionStateStore Store
+    {
+      get
+      {
+        System.Diagnostics.Debug.Assert(null != this.m_Store);
+
+        return this.m_Store;
+      }
+    }
+
+    #endregion
+
+    #region Public methods
+
+    public override void CreateUninitializedItem(HttpContext context, string id, int timeout)
+    {
+      Assert.ArgumentNotNull(context, "context");
+      Assert.ArgumentNotNull(id, "id");
+
+      const int flags = (int) SessionStateActions.InitializeItem;
+      var sessionItems = new SessionStateItemCollection();
+      var staticObjects = new HttpStaticObjectsCollection();
+      var sessionState = new SessionStateStoreData(sessionItems, staticObjects, timeout);
+
+      this.m_Store.InsertItem(this.ApplicationId, id, flags, sessionState);
+    }
+
+    public override SessionStateStoreData GetItem(HttpContext context, string id, out bool locked, out TimeSpan lockAge,
+      out object lockId, out SessionStateActions actions)
+    {
+      Assert.ArgumentNotNull(context, "context");
+      Assert.ArgumentNotNull(id, "id");
+
+      locked = false;
+      lockAge = TimeSpan.Zero;
+      lockId = null;
+      actions = SessionStateActions.None;
+
+      int flags = 0;
+      SessionStateLockCookie lockCookie = null;
+
+      SessionStateStoreData result = this.Store.GetItem(this.ApplicationId, id, out lockCookie, out flags);
+
+      actions = (SessionStateActions) flags;
+
+      if (null != lockCookie)
+      {
+        locked = true;
+        lockId = lockCookie.Id;
+        lockAge = DateTime.UtcNow - lockCookie.Timestamp;
+      }
+
+      return result;
+    }
+
+    public override SessionStateStoreData GetItemExclusive(HttpContext context, string id, out bool locked,
+      out TimeSpan lockAge, out object lockId, out SessionStateActions actions)
+    {
+      Assert.ArgumentNotNull(context, "context");
+      Assert.ArgumentNotNull(id, "id");
+
+      // Set the initial values of the out parameters. These are the values to be returned if the requested session
+      // state store item does not exist.
+      lockAge = TimeSpan.Zero;
+      actions = SessionStateActions.None;
+
+      int flags = 0;
+      SessionStateLockCookie existingLockCookie = null;
+      SessionStateLockCookie acquiredLockCookie = SessionStateLockCookie.Generate(DateTime.UtcNow);
+
+      SessionStateStoreData result =
+        this.Store.GetItemExclusive(this.ApplicationId, id, acquiredLockCookie, out existingLockCookie, out flags);
+
+      if (existingLockCookie != null)
+      {
+        locked = true;
+        lockAge = DateTime.UtcNow - existingLockCookie.Timestamp;
+        lockId = existingLockCookie.Id; // ??
+      }
+      else
+      {
+        locked = false;
+        lockId = acquiredLockCookie.Id; // ??
+        actions = (SessionStateActions) flags;
+      }
+
+      return result;
+    }
+
+    public override void Initialize(string name, NameValueCollection config)
+    {
+      Assert.ArgumentNotNull(name, "name");
+      Assert.ArgumentNotNull(config, "config");
+
+      base.Initialize(name, config);
+
+      var configuration = new ConfigReader(config, name);
+
+      string applicationName = configuration.GetString("sessionType", true);
+      string connectionName = configuration.GetString("connectionStringName", false);
+      string connectionString = ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+
+      bool compression = configuration.GetBool("compression", false);
+
+      this.m_Store = new SqlSessionStateStore(connectionString, compression);
+      this.m_ApplicationId = this.m_Store.GetApplicationIdentifier(applicationName);
+
+      lock (ListSyncRoot)
+      {
+        SqlSessionStateProvidersList.Add(this);
+      }
+      this.CanStartTimer = this.IsTimerOffForAllInstance;
+    }
+
+    public override void Dispose()
+    {
+      lock (ListSyncRoot)
+      {
+        SqlSessionStateProvidersList.Remove(this);
+        if (this.TimerEnabled && SqlSessionStateProvidersList.Count > 0)
+        {
+          foreach (var sqlSessionStateProvider in SqlSessionStateProvidersList)
+          {
+            if (sqlSessionStateProvider.ApplicationId == this.ApplicationId &&
+                sqlSessionStateProvider.TriedToStartTimer)
+            {
+              sqlSessionStateProvider.StartTimer();
+              break;
+            }
+          }
+        }
+      }
+      base.Dispose();
+    }
+
+    public override void ReleaseItemExclusive([NotNull] HttpContext context, string id, object lockId)
+    {
+      Assert.ArgumentNotNull(context, "context");
+      Assert.ArgumentNotNull(id, "id");
+      Assert.ArgumentNotNull(lockId, "lockId");
+
+      string lockCookie = Convert.ToString(lockId);
+
+      this.Store.ReleaseItem(this.ApplicationId, id, lockCookie);
+    }
+
+    public override void RemoveItem(HttpContext context, string id, object lockId, SessionStateStoreData item)
+    {
+      Assert.ArgumentNotNull(context, "context");
+      Assert.ArgumentNotNull(id, "id");
+      Assert.ArgumentNotNull(lockId, "lockId");
+
+      string lockCookie = Convert.ToString(lockId);
+
+      try
+      {
+        this.ExecuteSessionEnd(id, item);
+      }
+      finally
+      {
+        this.Store.RemoveItem(this.ApplicationId, id, lockCookie);
+      }
+    }
+
+    public override void ResetItemTimeout(HttpContext context, string id)
+    {
+      Assert.ArgumentNotNull(context, "context");
+      Assert.ArgumentNotNull(id, "id");
+
+      this.Store.UpdateItemExpiration(this.ApplicationId, id);
+    }
+
+    public override void SetAndReleaseItemExclusive([NotNull] HttpContext context, string id,
+      SessionStateStoreData sessionState, object lockId, bool newItem)
+    {
+      Assert.ArgumentNotNull(context, "context");
+      Assert.ArgumentNotNull(id, "id");
+
+      if (newItem)
+      {
+        const int flags = (int) SessionStateActions.None;
+        this.Store.InsertItem(this.ApplicationId, id, flags, sessionState);
+      }
+      else
+      {
+        string lockCookie = Convert.ToString(lockId);
+        this.Store.UpdateAndReleaseItem(this.ApplicationId, id, lockCookie, SessionStateActions.None, sessionState);
+      }
+    }
+
+    #endregion
+
+    #region Protected methods
+
+    protected override SessionStateStoreData GetExpiredItemExclusive(DateTime signalTime,
+      SessionStateLockCookie lockCookie, out string id)
+    {
+      return this.Store.GetExpiredItemExclusive(this.ApplicationId, lockCookie, out id);
+    }
+
+    protected override void RemoveItem(string id, string lockCookie)
+    {
+      this.Store.RemoveItem(this.ApplicationId, id, lockCookie);
+    }
+
+    #endregion
+
+    #region Private methods
+
+    private bool IsTimerOffForAllInstance()
+    {
+      lock (ListSyncRoot)
+      {
+        foreach (var sqlSessionStateProvider in SqlSessionStateProvidersList)
+        {
+          if (sqlSessionStateProvider.ApplicationId == this.ApplicationId &&
+              sqlSessionStateProvider.TriedToStartTimer &&
+              sqlSessionStateProvider.TimerEnabled)
+            return false;
+        }
+        return true;
+      }
+    }
+  }
 }
+
+#endregion
